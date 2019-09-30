1be04c134f850809091c452f4d7669cab1b3098f
sifat23 <shifat76890@gmail.com>
2019-10-01 04:08:07 +0600

    first commit

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                    @foreach($questions as $question)
+                    <div class="media">
+                        <div class="media-body">
+                            <h4 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h4>
+                            <p>Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>,
+                            <small class="text-muted">{{ $question->created_date }}</small></p>
+                            <div class="">
+                                {{ str_limit($question->body, 250) }}
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    @endforeach
+
+                    {{ $questions->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
